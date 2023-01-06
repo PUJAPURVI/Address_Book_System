29cfce3761cc63580fbec06692027a0cea3a1688
PUJAPURVI <nerkarpuja@gmail.com>
2023-01-06 16:26:01 +0530

    Added: Ability to add new contact

--- AddressBookSystem/AddressBook.java
+++ AddressBookSystem/AddressBook.java
@@ -1,107 +1,44 @@
 package com.AddressBookSystem;
 import java.util.Scanner;
+
 public class AddressBook {
+    //Scanner object for User Input
+    Scanner sc = new Scanner(System.in);
 
-    static String Firstname;
-    static String Lastname;
-    static String Address;
-    static String City;
-    static String State;
-    static int Zipcode;
-    static long Phone_no;
-    static String Email;
-    public String getFirstname() {
-        return Firstname;
+    //Created Contacts Object for Getter and Setter Methods
+    Contacts contacts = new Contacts();
+
+    //Method to Add New Contact to AddressBook
+    public void addNewContact() {
+        System.out.println("Enter the Contact Details -");
+        System.out.println("Enter the First Name :");
+        contacts.setFirstName(sc.next());
+        System.out.println("Enter the Last Name :");
+        contacts.setLastName(sc.next());
+        System.out.println("Enter the Address :");
+        contacts.setAddress(sc.next());
+        System.out.println("Enter the City :");
+        contacts.setCity(sc.next());
+        System.out.println("Enter the State :");
+        contacts.setState(sc.next());
+        System.out.println("Enter the Zip Code :");
+        contacts.setZip(sc.nextInt());
+        System.out.println("Enter the Phone Number :");
+        contacts.setPhoneNumber(sc.nextInt());
+        System.out.println("Enter the EMail ID :");
+        contacts.setEmail(sc.next());
     }
 
-    public void setFirstname(String firstname) {
-        Firstname = firstname;
-    }
-
-    public String getLastname() {
-        return Lastname;
-    }
-
-    public void setLastname(String lastname) {
-        Lastname = lastname;
-    }
-
-    public String getAddress() {
-        return Address;
-    }
-
-    public void setAddress(String address) {
-        Address = address;
-    }
-
-    public String getCity() {
-        return City;
-    }
-
-    public void setCity(String city) {
-        City = city;
-    }
-
-    public String getState() {
-        return State;
-    }
-    public void setState(String state) {
-        State = state;
-    }
-    public int getZipcode() {
-        return Zipcode;
-    }
-    public void setZipcode(int zipcode) {
-        Zipcode = zipcode;
-    }
-    public long getPhoneno() {
-        return Phone_no;
-    }
-
-    public void setPhoneno(long phoneno) {
-        Phone_no = (int) phoneno;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Enter First name :");
-        Scanner N = new Scanner(System.in);
-        Firstname = N.nextLine();
-
-        System.out.println("Enter Last name :");
-        Scanner L = new Scanner(System.in);
-        Lastname = L.nextLine();
-
-        System.out.println("Enter Address :");
-        Scanner A = new Scanner(System.in);
-        Address = A.nextLine();
-
-        System.out.println("Enter City :");
-        Scanner C = new Scanner(System.in);
-        City = C.nextLine();
-
-        System.out.println("Enter State :");
-        Scanner s = new Scanner(System.in);
-        State = s.nextLine();
-
-        System.out.println("Enter Zipcode :");
-        Scanner Z = new Scanner(System.in);
-        Zipcode = Z.nextInt();
-
-        System.out.println("Enter Phone no :");
-        Scanner P = new Scanner(System.in);
-        Phone_no = (int) P.nextLong();
-
-        System.out.println("Enter Email :");
-        Scanner E = new Scanner(System.in);
-        Email = E.nextLine();
+    //Method to Show the Contact Details
+    public void displayContact() {
+        System.out.println("Contact Details -");
+        System.out.println("First Name : "+contacts.getFirstName());
+        System.out.println("Last Name : "+contacts.getLastName());
+        System.out.println("Address : "+contacts.getAddress());
+        System.out.println("City : "+contacts.getCity());
+        System.out.println("State : "+contacts.getState());
+        System.out.println("Zip Code : "+contacts.getZip());
+        System.out.println("Phone Number : "+contacts.getPhoneNumber());
+        System.out.println("EMail ID : "+contacts.getEmail());
     }
 }
-
